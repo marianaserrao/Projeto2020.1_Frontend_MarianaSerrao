12ad3b36b6471f9f3503d07e2567fb0d3048e809
Mariana Serrão Guilherme <mserraoguilherme@gmail.com>
2020-04-17 00:10:24 -0300

    finish feed page

--- js/feed.js
+++ js/feed.js
@@ -1,101 +1,228 @@
 //elementos HTML
     var botaopiar = document.querySelector("#botaopiar");
-    var formpublucacao = document.querySelector("#publicar");
     var botaopesquisar = document.querySelector("#botaopesquisa");
     var campofiltro = document.querySelector("#pesquisar");
+    var formpublicacao = document.querySelector("#publicar");
     var contador= document.querySelectorAll(".contador");
     var erropublicacao = document.querySelector("#erropubli");
-    var feed = document.querySelector("#feed")
+    var feed = document.querySelector("#feed");
     var formpesquisa = document.querySelector("#formpesquisa");
     var erropesquisa = document.querySelector("#erropesq");
+    var botaomenu = document.querySelector("#botaomenu");
+    var header = document.querySelector("header");
+    var body= document.querySelector("body");
+    var main = document.querySelector("main");
 
 //funcao carregar pius
-    window.onload= function(){
+window.onload= function(){
 
-        var xhr = new this.XMLHttpRequest();
+    var xhr = new this.XMLHttpRequest();
 
-        xhr.open("GET", "https://next.json-generator.com/api/json/get/EkyZfHLU_")
+    xhr.open("GET", "https://next.json-generator.com/api/json/get/EkyZfHLU_")
 
-        xhr.addEventListener("load",function(){
-            var resposta = xhr.responseText;
-            var pius = JSON.parse(resposta);
+    xhr.addEventListener("load",function(){
+        var resposta = xhr.responseText;
+        var pius = JSON.parse(resposta);
             
-            pius.forEach(function(){
+        pius.forEach(function(piu){
 
-                console.log(this.nome);
+            //criando elementos
+            var div = document.createElement("div");
+            var usuario = document.createElement('div');
+            var imagem = document.createElement('div');
+            var nomes = document.createElement('div');
+            var user=document.createElement('h2')
+            var nome= document.createElement('h3');
+            var p = document.createElement("p");
+            var interacao = document.createElement("div");
+            var curtidas= document.createElement("p");
+            var curtir=document.createElement("button");
+            var del =document.createElement("button");
 
-                //criando elementos
-                var div = document.createElement("div");
-                var usuario = document.createElement('div');
-                var imagem = document.createElement('div');
-                var nomes = document.createElement('div');
-                var user=document.createElement('h2')
-                var nome= document.createElement('h3');
-                var p = document.createElement("p");
-                var interacao = document.createElement("div");
-                var curtidas= document.createElement("p");
-                var curtir=document.createElement("button");
-                var del =document.createElement("button");
+            //preenchendo elementos
+            var img=piu.imagem
+            if(img!=0){
+                imagem.style.backgroundImage= "url('"+img+"')";
+            }
+            nome.textContent=piu.nome;
+            user.textContent=piu.username;
+            p.textContent=piu.mensagem;
+            curtidas.textContent="0";
+            curtir.textContent="Curtir";
+            del.textContent=" Deletar";
 
-                //preenchendo elementos
-                imagem.style.backgroundImage= "url("pius.imagem")";
-                nome.textContent=this.nome;
-                user.textContent=this.username;
-                p.textContent=this.mensagem;
-                curtidas.textContent="0";
-                curtir.textContent="Curtir";
-                del.textContent=" Deletar";
-
-                //envelopando elementos
-                interacao.appendChild(curtidas);
-                interacao.appendChild(curtir);
-                interacao.appendChild(del);
-                nomes.appendChild(nome);
-                nomes.appendChild(user);
-                usuario.appendChild(imagem);
-                usuario.appendChild(nomes);
-                div.appendChild(usuario);
-                div.appendChild(p);
-                div.appendChild(interacao);
-                feed.prepend(div);
+            //envelopando elementos
+            interacao.appendChild(curtidas);
+            interacao.appendChild(curtir);
+            interacao.appendChild(del);
+            nomes.appendChild(nome);
+            nomes.appendChild(user);
+            usuario.appendChild(imagem);
+            usuario.appendChild(nomes);
+            div.appendChild(usuario);
+            div.appendChild(p);
+            div.appendChild(interacao);
+            feed.prepend(div);
                 
-                //classificando elementos
-                div.classList.add("divfeed");
-                usuario.classList.add("divusuario");
-                nome.classList.add("nome");
-                user.classList.add("user")
-                imagem.classList.add('imagem');
-                p.classList.add("piu");
-                interacao.classList.add("interacao");
-                curtidas.classList.add("curtidas");
-                curtir.classList.add("curtir");
-                del.classList.add("del");
+            //classificando elementos
+            div.classList.add("divfeed");
+            usuario.classList.add("divusuario");
+            nome.classList.add("nome");
+            user.classList.add("user")
+            imagem.classList.add('imagem');
+            p.classList.add("piu");
+            interacao.classList.add("interacao");
+            curtidas.classList.add("curtidas");
+            curtir.classList.add("curtir");
+            del.classList.add("del");
 
-                //funcao curtir
-                var i = 0;
-                var curtido=false;
-                
-                curtir.addEventListener("click",function(){
-                    event.preventDefault();
+            //funcao curtir
+            var i = 0;
+            var curtido=false;
+            
+            curtir.addEventListener("click",function(){
+                event.preventDefault();
                     
-                    if(curtido==false){
-                        i+=1;
-                        curtidas.textContent=i;
-                        curtido=true;
-                    }
-                })
-                
-                //funcao deletar
-                del.addEventListener("click",function(){
-                    event.preventDefault();
-                    div.remove();
-                })    
-                
+                if(curtido==false){
+                    i+=1;
+                    curtidas.textContent=i;
+                    curtido=true;
+                }
             })
-        });
+                
+            //funcao deletar
+            del.addEventListener("click",function(){
+                event.preventDefault();
+                div.remove();                    
+            })    
+            
+        })
+    });
 
-        xhr.send();
+    xhr.send();
+}
+
+var menu=false
+
+//funcao abrir menu
+botaomenu.addEventListener('click',function(){
+    event.preventDefault;
+    
+    if(menu==false){
+        var opcoes= document.createElement('div');
+        var perfil= document.createElement("button");
+        var ajuda = document.createElement("button")
+        var sair= document.createElement('button');
+
+        perfil.textContent="Perfil";
+        ajuda.textContent="Ajuda";
+        sair.textContent="Sair";
+
+        opcoes.classList.add('divopcoes');
+        perfil.classList.add("botaoperfil");
+        ajuda.classList.add("botaoajuda");
+        sair.classList.add("botaosair");
+
+        opcoes.appendChild(perfil);
+        opcoes.appendChild(ajuda);
+        opcoes.appendChild(sair);
+        header.appendChild(opcoes);
+
+        perfil.addEventListener("click",function(){
+            window.location='index-perfil.html';
+        })
+
+        ajuda.addEventListener("click",function(){
+            window.location='index-ajuda.html';
+        })
+
+        sair.addEventListener("click",function(){
+            window.location='index-login.html';
+        })
+       
+        menu=true;
     }
+
+})
+
+//funcao fechar menu
+main.addEventListener("click",function(){
+
+    if (menu==true){
+        var opcoes=document.querySelector(".divopcoes");
+        opcoes.remove();
+        menu=false;
+    }
+    
+})
+
+//funcao filtrar
+campofiltro.addEventListener("input",function(){
+
+    erropesquisa.innerHTML='';
+
+    var pius=document.querySelectorAll('.piu');
+    var nomes=document.querySelectorAll(".nome");
+    var users=document.querySelectorAll(".user");
+    var divsfeed=document.querySelectorAll(".divfeed");
+
+    for(i=0; i<pius.length; i++){
+
+        piu=pius[i].textContent;
+        nome=nomes[i].textContent;
+        user=users[i].textContent;
+        divfeed=divsfeed[i];
+        var expressao = new RegExp(this.value, "i");
+
+        if (!expressao.test(piu)&&!expressao.test(nome)&&!expressao.test(user)){
+            divfeed.style.display="none"; 
+        }
+        if(expressao.test(piu)||expressao.test(nome)||expressao.test(user)){
+            divfeed.style.display="inherit";
+        }
+    }
+
+})
+
+//funcao pesquisar
+botaopesquisar.addEventListener("click",function(){
+    event.preventDefault();
+    
+    //limpando erros
+    erropesquisa.innerHTML='';
+    
+    //validando inputs
+    var invalida = false
+    var input=formpesquisa.pesquisar.value;
+    var piu=i.novopiu.value;
+    
+    if (input==0){
+        erropesquisa.textContent= "Pesquisa Inválida!";
+        invalida =true
+    }
+    if(input!=0){
+        
+        var pius=document.querySelectorAll('.piu');
+        var nomes=document.querySelectorAll(".nome");
+        var users=document.querySelectorAll(".user");
+        var divsfeed=document.querySelectorAll(".divfeed");
+        
+        for(i=0; i<pius.length; i++){
+
+            piu=pius[i].textContent;
+            nome=nomes[i].textContent;
+            user=users[i].textContent;
+            divfeed=divsfeed[i];
+            var expressao = new RegExp(campofiltro.value, "i");
+            
+            if (!expressao.test(piu)&&!expressao.test(nome)&&!expressao.test(user)){
+                
+                erropesquisa.textContent= "Nenhum resultado encontrado";
+
+            }
+        }
+    }
+})    
 
 //funcao contador caracteres
 function caracteres(contar,mostrar) {
@@ -133,7 +260,7 @@ botaopiar.addEventListener("click",function(){
     
     //validando inputs
     var invalido=false;
-    var piu=formpublucacao.novopiu.value;
+    var piu=i.novopiu.value;
     
     if (piu==0){
         erropublicacao.textContent="Piu Inválido!";
@@ -148,6 +275,7 @@ botaopiar.addEventListener("click",function(){
     if(invalido==false){
         
         //preenchendo elementos
+        imagem.backgroundImage="url(+img/usuario.png+)"
         nome.textContent="Mariana Serrão";
         user.textContent="@MariS"
         p.textContent=piu;
@@ -180,7 +308,7 @@ botaopiar.addEventListener("click",function(){
         curtir.classList.add("curtir");
         del.classList.add("del");
 
-        formpublucacao.reset();
+        i.reset();
     }
     
     //funcao curtir
@@ -205,36 +333,7 @@ botaopiar.addEventListener("click",function(){
     
 })
 
-//funcao filtrar
-campofiltro.addEventListener("input",function(){
 
-    console.log(campofiltro.value);
-    var pius=document.querySelectorAll('.piu');
-    for(i=0; i<pius.length; i++){
-
-        piu=pius[i];
-        console.log(piu);
-    }
-
-})
-
-//funcao pesquisar
-botaopesquisar.addEventListener("click",function(){
-    event.preventDefault();
-    
-    //limpando erros
-    erropesquisa.innerHTML='';
-    
-    //validando inputs
-    var invalida = false
-    var input=formpesquisa.pesquisar.value;
-    var piu=formpublucacao.novopiu.value;
-    
-    if (input==0){
-        erropesquisa.textContent= "Pesquisa Inválida!";
-        invalida =true
-    }
-})
 
 
 
